009d358d851098a591092c9b9c4abbd4d8a56d06
kurosiba <akito030303@gmail.com>
2020-06-22 15:35:34 +0900

    hello world

--- mysql/db/init.sql
+++ mysql/db/init.sql
@@ -1,10 +1,9 @@
-CREATE DATABASE gameapi_db;
-use gameapi_db;
+CREATE DATABASE test_db;
+use test_db;
 
 CREATE TABLE user (
-    id integer(10), 
-    username varchar(255), 
-    token varchar(255)); 
+    
+    username varchar(255)); 
 
 
-INSERT INTO user (id, username, token) VALUES ( 1, 'kawauchiya' , 1);
+INSERT INTO user (username) VALUES ('example');
--- src/main.go
+++ src/main.go
@@ -1,7 +1,20 @@
+//print "Hello world"
+//if you run, change filename "main.go"
+
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request){
+	fmt.Fprintf(w, "Hello, World")
+}
 
 func main(){
-	fmt.Printf("Hello World\n")
+	//fmt.Printf("Hello World\n")
+	//run 8080 port
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
 }
