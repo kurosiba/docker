c81eca16e473ef59c97d1c374832af1ff24107be
kurosiba <akito030303@gmail.com>
2020-07-08 14:38:47 +0900

    [add]one line get and all line get from table users

--- mysql/db/init.sql
+++ mysql/db/init.sql
@@ -1,9 +1,11 @@
 CREATE DATABASE test_db;
 use test_db;
 
-CREATE TABLE user (
-    
-    username varchar(255)); 
+CREATE TABLE users (
+    username varchar(255),
+    token integer(10)
+    ); 
 
-
-INSERT INTO user (username) VALUES ('example');
+INSERT INTO users (username, token) VALUES ('example1', 1);
+INSERT INTO users (username, token) VALUES ('example2', 2);
+INSERT INTO users (username, token) VALUES ('example3', 3);
--- src/main.go
+++ src/main.go
@@ -1,20 +1,90 @@
-//print "Hello world"
-//if you run, change filename "main.go"
 
 package main
 
 import (
 	"fmt"
 	"net/http"
+  "log"
+  "database/sql"
+   _ "github.com/go-sql-driver/mysql"
+
 )
 
-func handler(w http.ResponseWriter, r *http.Request){
+func HelloHandler(w http.ResponseWriter, r *http.Request){
 	fmt.Fprintf(w, "Hello, World")
 }
 
-func main(){
-	//fmt.Printf("Hello World\n")
-	//run 8080 port
-	http.HandleFunc("/", handler)
-	http.ListenAndServe(":8080", nil)
+func GetHandler(w http.ResponseWriter, r *http.Request){
+  
+  db, err := sql.Open("mysql", "root:root@tcp(mysql_test:3306)/test_db")
+
+  if err != nil {
+  panic(err)
 }
+defer db.Close()
+
+var (
+  name string
+  token int
+)
+
+if err := db.QueryRow("SELECT username,token FROM users WHERE token=?", 2).Scan(&name,&token);
+
+err != nil {
+log.Fatal(err)
+}
+
+log.Println(name, token)
+  
+}
+
+func AllGetHandler(w http.ResponseWriter, r *http.Request){
+
+  db, err := sql.Open("mysql", "root:root@tcp(mysql_test:3306)/test_db")
+
+  if err != nil {
+  panic(err)
+}
+defer db.Close()
+
+rows, err := db.Query("SELECT username, token FROM users")
+if err != nil{
+  log.Fatal(err)
+}
+defer rows.Close()
+
+for rows.Next(){
+  var token int
+  var name string
+
+  if err := rows.Scan(&name,&token);
+  err != nil{
+    log.Fatal(err)
+  }
+log.Println(name, token)
+}
+
+if err := rows.Err();
+err != nil{
+  log.Fatal(err)
+}
+
+}
+
+func main(){
+  http.HandleFunc("/", HelloHandler)
+  http.HandleFunc("/get", GetHandler)
+  http.HandleFunc("/getAll",AllGetHandler)
+  /*when enter "http://localhost:8080/get" url, it send GerRequest to server*/
+  //http.HandleFunc("/post", PostHandler)
+  //http.HandleFunc("/put", PutHandler)
+
+  http.ListenAndServe(":8080", nil)
+  
+}
+
+
+
+
+
+
